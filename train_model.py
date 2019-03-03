34fb6355bfe22ae8ba1c245ad85dc3c45c11b712
jinghu <jinghul@bu.edu>
2019-03-03 22:20:31 +0800

    feat selected

--- train_model.py
+++ train_model.py
@@ -18,6 +18,7 @@ from nltk.corpus import stopwords
 #import simplejson as json
 # from sentiment_analyzer import SentimentIntensityAnalyzer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import KFold, GridSearchCV
 from itertools import product
 from inspect import getsourcefile
@@ -144,7 +145,7 @@ if __name__ == "__main__":
     #     'gamma' : [0.01, 0.1, 1, 2, 3]
     # }
     # classifier = GridSearchCV(SVC(), param_grid)
-    classifier = SVC(C=1, gamma=1)
+    # classifier = SVC(C=1, gamma=1)
 
     print("Loading data...")
     # with open(os.path.join(data_dir, 'tweets_processed.txt'), 'r') as f:
@@ -154,66 +155,62 @@ if __name__ == "__main__":
     with open(os.path.join(data_dir, 'labels.txt'), 'r') as f:
         y = np.array([int(line.strip()) for line in f.readlines()])
 
+    classifier = MultinomialNB(alpha=0.25)
+
     # Feature Selection
-    x_feats_orig = TfidfVectorizer().fit_transform(x)
-
-    def feat_sel_val(percent):
-        global x_feats_orig, classifier
-        f_selector = SelectPercentile(f_classif, percentile=percent)
-        f_selector.fit(x_feats_orig, y)
-        x_feats = f_selector.transform(x_feats_orig).toarray()
-        print(x_feats.shape)
-
-        print("Start training and predict...")
-        kf = KFold(n_splits=10)
-        avg_p_all, avg_p_pos, avg_p_neg, avg_p_net = 0, 0, 0, 0
-        avg_r_all, avg_r_pos, avg_r_neg, avg_r_net = 0, 0, 0, 0
-        cnt = 0
-        for train, test in kf.split(y):
-
-            model = classifier.fit(x_feats[train], y[train])
-            # print('Curr best svm params: %d' % classifier.best_params_, end='\n')
-            predicts = model.predict(x_feats[test])
-
-            # predict_scores = []
-            # for ind in test:
-            #     # for instance in x[test]:
-            #       instance = x[ind]
-            #       predict_scores.append(classifier.polarity_scores(instance)["compound"])
-            # predicts = sentiment_analyzer.map_to_label(predict_scores)
-            # print(classification_report(y[test], predicts))
-            
-            # pos = label 2
-            avg_p_pos += precision_score(y[test], predicts, labels=[2], average='macro')
-            avg_r_pos += recall_score(y[test], predicts, labels=[2], average='macro')
-
-            # neutral = label 1
-            avg_p_net += precision_score(y[test], predicts, labels=[1], average='macro')
-            avg_r_net += recall_score(y[test], predicts, labels=[1], average='macro')
-
-            # negative = label 0
-            avg_p_neg += precision_score(y[test], predicts, labels=[0], average='macro')
-            avg_r_neg += recall_score(y[test], predicts, labels=[0], average='macro')
-
-            avg_p_all += precision_score(y[test], predicts, average='macro')
-            avg_r_all += recall_score(y[test], predicts, average='macro')
-
-            cnt += 1
-            print('Fold %d completed.' % cnt, end='\n')
-
-        print('Average Positive Precision is %f' % (avg_p_pos / 10.0))
-        print('Average Positive Recall is %f' % (avg_r_pos / 10.0), end='\n')
-        print('--------------------', end='\n')
-        print('Average Neutral Precision is %f' % (avg_p_net / 10.0))
-        print('Average Neutral Recall is %f' % (avg_r_net / 10.0), end='\n')
-        print('--------------------', end='\n')
-        print('Average Negative Precision is %f' % (avg_p_neg / 10.0))
-        print('Average Negative Recall is %f' % (avg_r_neg / 10.0), end='\n')
-        print('--------------------', end='\n')
-        print('Average Total Precision is %f' % (avg_p_all / 10.0))
-        print('Average Total Recall is %f' % (avg_r_all / 10.0), end='\n')
+    x_feats = TfidfVectorizer().fit_transform(x)
 
     from sklearn.feature_selection import SelectPercentile, f_classif
-    for i in [20, 30]:
-        print("Percent features %d" % i, end='\n')
-        feat_sel_val(i)
+    f_selector = SelectPercentile(f_classif, percentile=60)
+    f_selector.fit(x_feats, y)
+    x_feats = f_selector.transform(x_feats).toarray()
+    print(x_feats.shape)
+
+    print("Start training and predict...")
+    kf = KFold(n_splits=10)
+    avg_p_all, avg_p_pos, avg_p_neg, avg_p_net = 0, 0, 0, 0
+    avg_r_all, avg_r_pos, avg_r_neg, avg_r_net = 0, 0, 0, 0
+    cnt = 0
+    for train, test in kf.split(y):
+
+        model = classifier.fit(x_feats[train], y[train])
+        # print('Curr best svm params: %d' % classifier.best_params_, end='\n')
+        predicts = model.predict(x_feats[test])
+
+        # predict_scores = []
+        # for ind in test:
+        #     # for instance in x[test]:
+        #       instance = x[ind]
+        #       predict_scores.append(classifier.polarity_scores(instance)["compound"])
+        # predicts = sentiment_analyzer.map_to_label(predict_scores)
+        # print(classification_report(y[test], predicts))
+        
+        # pos = label 2
+        avg_p_pos += precision_score(y[test], predicts, labels=[2], average='macro')
+        avg_r_pos += recall_score(y[test], predicts, labels=[2], average='macro')
+
+        # neutral = label 1
+        avg_p_net += precision_score(y[test], predicts, labels=[1], average='macro')
+        avg_r_net += recall_score(y[test], predicts, labels=[1], average='macro')
+
+        # negative = label 0
+        avg_p_neg += precision_score(y[test], predicts, labels=[0], average='macro')
+        avg_r_neg += recall_score(y[test], predicts, labels=[0], average='macro')
+
+        avg_p_all += precision_score(y[test], predicts, average='macro')
+        avg_r_all += recall_score(y[test], predicts, average='macro')
+
+        cnt += 1
+        print('Fold %d completed.' % cnt, end='\n')
+
+    print('Average Positive Precision is %f' % (avg_p_pos / 10.0))
+    print('Average Positive Recall is %f' % (avg_r_pos / 10.0), end='\n')
+    print('--------------------', end='\n')
+    print('Average Neutral Precision is %f' % (avg_p_net / 10.0))
+    print('Average Neutral Recall is %f' % (avg_r_net / 10.0), end='\n')
+    print('--------------------', end='\n')
+    print('Average Negative Precision is %f' % (avg_p_neg / 10.0))
+    print('Average Negative Recall is %f' % (avg_r_neg / 10.0), end='\n')
+    print('--------------------', end='\n')
+    print('Average Total Precision is %f' % (avg_p_all / 10.0))
+    print('Average Total Recall is %f' % (avg_r_all / 10.0), end='\n')
