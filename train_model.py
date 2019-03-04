19fb0dcff6f518af54ad92600355bddaee1f135c
jinghu <jinghul@bu.edu>
2019-03-04 16:34:22 +0800

    final

--- train_model.py
+++ train_model.py
@@ -190,7 +190,7 @@ if __name__ == "__main__":
     feats_union = FeatureUnion([ 
         ('tfidf', TfidfVectorizer()),
         ('senti', FunctionTransformer(get_senti_features, validate=False)),
-        ('len', FunctionTransformer(get_len_tweet, validate=False))
+        # ('len', FunctionTransformer(get_len_tweet, validate=False))
         # ('rts', FunctionTransformer(get_rts_counts, validate=False))
     ])
 
@@ -201,7 +201,7 @@ if __name__ == "__main__":
     print(x_feats.shape)
 
     # classifier = VotingClassifier(estimators=[('nb', MultinomialNB(alpha=0.25)), ('svm', SVC(C=1.0, gamma=1.0))])
-    classifier = SVC(C=1.0, gamma=1)
+    classifier = SVC(C=2.0, gamma=2)
     # classifier = MultinomialNB(alpha=0.3)
 
     print("Start training and predict...")
